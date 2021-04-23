2684f5f64e97c73507d3f58f444c538d2c23568b
c332030 <c332030@gmail.com>
2021-04-23 09:29:25 +0800

    feat: ssh ConnectTimeout

--- init-ssh.sh
+++ init-ssh.sh
@@ -2,19 +2,20 @@
 
 set -e
 
-if [ "" = "$SSH_PRIVATE_KEY" ]
-then
-  echo no SSH_PRIVATE_KEY
-  exit
-fi
-
 ROOT_SSH_PATH=/root/.ssh
-ROOT_ID_RSA=${ROOT_SSH_PATH}/id_rsa
-
 mkdir -p ${ROOT_SSH_PATH}
 
-echo "$SSH_PRIVATE_KEY" > ${ROOT_ID_RSA}
-chmod 600 ${ROOT_ID_RSA}
+echo "
+Host *
+    ConnectTimeout 10
+" >> ${ROOT_SSH_PATH}/config
+
+if [ -n "$SSH_PRIVATE_KEY" ]
+then
+  ROOT_ID_RSA=${ROOT_SSH_PATH}/id_rsa
+  echo "$SSH_PRIVATE_KEY" > ${ROOT_ID_RSA}
+  chmod 600 ${ROOT_ID_RSA}
+fi
 
 USER_SSH_PATH=~/.ssh
 mkdir -p ${USER_SSH_PATH}
